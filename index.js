2d9e8fd1b56fbae6b309a25525dc89e69fc98634
Luandro <luandro@gmail.com>
2016-06-04 00:38:13 -0400

    payment api

--- index.js
+++ index.js
@@ -5,11 +5,26 @@ var cors = require('cors');
 var app = express();
 var port = process.env.PORT || 3001;
 var WooCommerceAPI = require('woocommerce-api');
+var pagseguro = require('pagseguro');
+var XMLparser = require('xml2json');
+var uuid = require('node-uuid').v4;
+
+
 var configUrl = process.env.URL;
 var configConsumerKey = process.env.CONSUMERKEY;
 var configConsumerSecret = process.env.CONSUMERSECRET;
+var configToken = process.env.TOKEN;
+var configEmail = process.env.EMAIL;
 
-var whitelist = ['http://dev.repsparta.com', 'http://repsparta.com', 'https://repsparta.com', 'http://locahost:300'];
+var whitelist = [
+  'http://alpha.repsparta.com',
+  'http://beta.repsparta.com',
+  'http://repsparta.com',
+  'https://repsparta.com',
+  'http://locahost:300',
+  'http://dev.repsparta.com'
+];
+
 var corsOptions = {
   origin: function(origin, callback){
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
@@ -23,16 +38,68 @@ var WooCommerce = new WooCommerceAPI({
   consumerSecret: configConsumerSecret
 });
 
+var pag = new pagseguro({
+  email : configEmail,
+  token: configToken,
+  mode : 'sandbox'
+});
+
+/**
+ * Hello
+*/
 app.get('/hello', function(req, res) {
     console.log(configUrl)
 console.log(configConsumerKey)
     res.send('YYYYYYoooooooo!');
-})
+});
+/**
+ * Payment
+ */
+ app.post('/api/payment', cors(corsOptions), (req, res) => {
+   console.log('acessing /payment POST');
+   console.log(req.body);
+   const data = req.body.order;
+   data.line_items.map((item) => {
+     pag.addItem({
+         id: item.id,
+         description: item.name,
+         amount: item.price+'.00',
+         quantity: item.quantity,
+         weight: ''
+     });
+   });
+   pag.currency('BRL');
+   pag.setRedirectURL("http://loja.repsparta.com");
+   pag.setNotificationURL("http://loja.repsparta.com/shop");
+   pag.reference(uuid());
+   pag.buyer({
+       name: data.billing_address.first_name,
+       email: data.billing_address.email,
+       country: 'BRA'
+   });
+   pag.send((err, payRes) => {
+     if (err) {
+       console.log(err);
+     }
+     const formatedData = XMLparser.toJson(payRes, {object: true});
+     console.log('Payment response', formatedData);
+     if(formatedData.checkout) {
+       res.status(200).json({
+         ok: true,
+         code: formatedData.checkout.code
+       });
+     } else {
+       res.status(404).json({
+         ok: false
+       })
+     }
+   });
+ });
 
 /**
  * Products API
  */
-app.get('/products', cors(corsOptions), function (req, res) {
+app.get('/api/products', cors(corsOptions), function (req, res) {
   console.log("Acessing /products");
   WooCommerce.get('products', function (err, data, response) {
     if (data.statusCode === 200) {
@@ -46,7 +113,7 @@ app.get('/products', cors(corsOptions), function (req, res) {
 /**
  * Orders API
  */
-app.post('/order', cors(corsOptions), function (req, res) {
+app.post('/api/order', cors(corsOptions), function (req, res) {
   console.log("Acessing /order POST", req.body);
   var data = req.body;
   WooCommerce.post('orders', data, function (error, data, wooRes) {
